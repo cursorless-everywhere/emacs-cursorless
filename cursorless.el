523dc309c2d35c950834903ae1bfb93d965298b1
Michael Arntzenius <daekharel@gmail.com>
2022-07-09 20:54:50 +0100

    work on cursorless.el

--- cursorless.el
+++ cursorless.el
@@ -1,13 +1,12 @@
-; a very simple test test test test test
 (require 'cl-macs)
 
-(defmacro measure-time (&rest body)
+(defmacro measure-time (name &rest body)
   "Measure the time it takes to evaluate BODY."
   `(let ((time (current-time)))
      (prog1 (progn ,@body)
-       (message "%.06f" (float-time (time-since time))))))
+       (message "%30s %3d ms" ',name (round (* 1000 (float-time (time-since time))))))))
 
-(defun line-and-column (pos)
+(defun line-and-cloumn (pos)
   ;; Note that (current-column) is wrong, we want # of characters since start of
   ;; line, NOT the logical position. (eg. tab counts as 1 char).
   ;; cursorless line numbers are 1-indexed. not sure about column numbers.
@@ -61,6 +60,7 @@
 (defun cursorless-disable ()
   (interactive)
   (setq cursorless-enabled nil)
+  ;; TODO: file-notify-rm-watch cursorless-hats-watcher?
   (cancel-timer cursorless-send-state-timer))
 
 ;;; Scrolling seems janky, but it doesn't look like we're causing it?
@@ -136,11 +136,20 @@
 ;; READING & DRAWING HATS FROM CURSORLESS ;;
 (defvar hats-buffer nil)
 
-(defun cursorless-hats-change-callback (event)
-  (message "hats change callback! v2")
+(defvar cursorless-hats-update-timer nil)
+(defun cursorless-hats-update-callback (&optional event)
+  (setq cursorless-hats-update-timer nil)
   (when hats-buffer (with-current-buffer hats-buffer (cursorless-update-hats))))
+(defun cursorless-hats-change-callback (&optional event)
+  (unless cursorless-hats-update-timer
+    (setq cursorless-hats-update-timer
+          (run-with-idle-timer 0.1 nil 'cursorless-hats-update-callback))))
+
 (defvar cursorless-hats-watcher
-  (file-notify-add-watch cursorless-hats-file '(change) 'cursorless-hats-change-callback))
+  (progn
+    (when (and (boundp 'cursorless-hats-watcher) cursorless-hats-watcher)
+      (file-notify-rm-watch cursorless-hats-watcher))
+    (file-notify-add-watch cursorless-hats-file '(change) 'cursorless-hats-change-callback)))
 
 ;; TODO: defcustom
 (defparam cursorless-color-alist
@@ -150,6 +159,7 @@
 
 (defun show-hats ()
   (interactive)
+  (when hats-buffer (with-current-buffer hats-buffer (cursorless-clear-overlays)))
   (setq hats-buffer (current-buffer))
   (cursorless-update-hats))
 
@@ -160,148 +170,220 @@
 
 (defun cursorless-clear-overlays ()
   (interactive)
-  (remove-overlays nil nil 'cursorless t))
+  (measure-time cursorless-clear-overlays
+   (remove-overlays nil nil 'cursorless t)))
 
 (defun cursorless-read-hats-json ()
-  (with-temp-buffer
-    (insert-file-contents-literally cursorless-hats-file)
-    (json-parse-buffer :object-type 'alist)))
-
-;;; NB. "A loop that scans the buffer forwards, creating overlays, can run faster if you do (overlay-recenter (point-max)) first."
-;;; https://www.gnu.org/software/emacs/manual/html_node/elisp/Managing-Overlays.html
+  (measure-time cursorless-read-hats-json
+   (with-temp-buffer
+     (insert-file-contents-literally cursorless-hats-file)
+     (json-parse-buffer :object-type 'alist))))
 
 
-;;; ---------- OLD VERSION ----------
+;; ;;; ---------- OLD VERSION ----------
+;; (defun cursorless-update-hats ()
+;;   (message " ")
+;;   (measure-time cursorless-update-hats
+;;     (cursorless-update-overlays (read-hats))))
+
+;; (defun read-hats ()
+;;   (measure-time read-hats
+;;    (let* ((hats (cursorless-read-hats-json))
+;;           (file-hats (cdar hats)))
+;;      ;; hats-json is an alist of the form ((file . file-hats) ...)
+;;      ;; file-hats are of the form: ((color . [hat ...]) ...)
+;;      ;; hats are of the form: ((start (line . n) (character . n)) (end (line . n) (character . c)))
+;;      ;;
+;;      ;; for now we assume there's only one file, so we just grab the cdar
+;;      ;; TODO: this ought to be much simpler.
+;;      (cl-loop for (color . hats) in file-hats
+;;               nconc (cl-loop for hat across hats
+;;                              collect (let ((x (alist-get 'start hat)))
+;;                                        (list color (alist-get 'line x) (alist-get 'character x))))))))
+
+;; (defun cursorless-update-overlays (hats)
+;;   (measure-time cursorless-update-overlays
+;;    (cursorless-clear-overlays)
+;;    (overlay-recenter (point))
+;;    (let ((images (cursorless-hat-images hats)))
+;;      (measure-time cursorless-add-overlays
+;;        (maphash 'cursorless-add-overlay-to-line images)))))
+
+;; (defvar cursorless-hat-svgs (make-hash-table))
+;; (defvar cursorless-hat-images (make-hash-table))
+;; (defun cursorless-hat-images (hats)
+;;   (measure-time cursorless-hat-images
+;;    (clrhash cursorless-hat-svgs)
+;;    (clrhash cursorless-hat-images)
+;;    (let* ((w (window-font-width))
+;;           (dia (* w 0.5)) (h (* 0.6 (window-font-height))) (r (/ dia 2)) (ypos (- h (* r 2)))
+;;           ;; (dia (* w .45)) (h (* w 0.8)) (r (/ dia 2)) (ypos (- h (* r 1.5)))
+;;           (dia (* w .44)) (r (* 0.5 dia)) (h (+ 2 (round dia))) (ypos (- h (* r 1) 1))
+;;           )
+;;      ;; hats is a list ((color line offset) (color line offset) ...)
+;;      (dolist (hat hats)
+;;        (cl-destructuring-bind (color line column) hat
+;;          (let* ((color (or (cdr (assoc color cursorless-color-alist))
+;;                            (symbol-name color)))
+;;                 (svg (or (gethash line cursorless-hat-svgs)
+;;                          (let ((columns (save-excursion
+;;                                           (goto-char (point-min))
+;;                                           (forward-line line)
+;;                                           (- (end-of-line-position) (point)))))
+;;                            (puthash line (svg-create (* w columns) h)
+;;                                     cursorless-hat-svgs)))))
+;;            (svg-circle svg (+ (* w column) (/ w 2.0)) ypos r :fill color))))
+;;      (cl-loop for line being the hash-keys of cursorless-hat-svgs
+;;                        using (hash-values svg)
+;;               do (puthash line (svg-image svg :scale 1) cursorless-hat-images))
+;;      cursorless-hat-images)))
+
+;; (defun cursorless-add-overlay-to-line (line svg)
+;;   (save-excursion
+;;     (goto-char (point-min))
+;;     (forward-line line)
+;;     (let* ((overlay (make-overlay (point) (point)))
+;;            (text (copy-sequence "x\n"))
+;;            (image svg))
+;;       (put-text-property 0 (length text) 'line-height t text)
+;;       (put-text-property 0 1 'display image text)
+;;       (overlay-put overlay 'cursorless t)
+;;       (overlay-put overlay 'before-string text))))
+
+
+;;; ---------- NEW VERSION ----------
+;; hats are stored as a JSON object like:
+;; {
+;;   FILEPATH: {
+;;     COLORNAME: [ { "start": { "line": l, "character": c },
+;;                    "end": { "line": l, "character": c } },
+;;                  ... more hat positions ... ]
+;;     ... more colors ...
+;;   },
+;;   ... more files ...
+;; }
+;;
+;; when hats have shapes, COLORNAME is altered (TODO: how).
 (defun cursorless-update-hats ()
-  (cursorless-update-overlays (read-hats)))
+  (message " ")
+  (measure-time cursorless-update-hats
+    (cursorless-update-overlays
+     (cursorless-index-hats
+      (cursorless-read-hats-json)))))
 
-(defun read-hats ()
-  (let* ((hats (cursorless-read-hats-json))
-         (file-hats (cdar hats)))
-    ;; hats-json is an alist of the form ((file . file-hats) ...)
-    ;; file-hats are of the form: ((color . [hat ...]) ...)
-    ;; hats are of the form: ((start (line . n) (character . n)) (end (line . n) (character . c)))
-    ;;
-    ;; for now we assume there's only one file, so we just grab the cdar
-    ;; TODO: this ought to be much simpler.
-    (cl-loop for (color . hats) in file-hats
-             nconc (cl-loop for hat across hats
-                            collect (let ((x (alist-get 'start hat)))
-                                      (list color (alist-get 'line x) (alist-get 'character x)))))))
+(defvar cursorless-hats (make-hash-table))
+(defun cursorless-index-hats (hats-json)
+  (measure-time cursorless-index-hats
+   (clrhash cursorless-hats)
+   (cl-loop
+    ;; for now we assume there's only one file, so we just grab the cdar.
+    for (color . hats) in (cdar hats-json)
+    do (cl-loop
+        for hat across hats
+        ;; assume each hat's start & end position are the same; we only use the
+        ;; start.
+        for position = (alist-get 'start hat)
+        for line     = (alist-get 'line position)
+        for column   = (alist-get 'character position)
+        for rest     = (gethash line cursorless-hats '())
+        for hat     = `(:column ,column
+                        :color ,(or (cdr (assoc color cursorless-color-alist))
+                                    (symbol-name color)))
+        do (puthash line (cons hat rest) cursorless-hats))))
+  cursorless-hats)
 
-(defvar cursorless-hat-images (make-hash-table))
-(defun cursorless-hat-images (hats)
-  (clrhash cursorless-hat-images)
-  (let* ((w (window-font-width))
-         (dia (* w 0.5)) (h (* 0.6 (window-font-height))) (r (/ dia 2)) (ypos (- h (* r 2)))
+(defun num-columns ()
+  (- (line-end-position) (point)))
+
+(defun cursorless-update-overlays (line-hats)
+  (cursorless-clear-overlays)
+  ;; for every line, generate an image.
+  ;; TODO: new strategy:
+  ;; 1. for each line, generate info we need to annotate it:
+  ;;    (line position num-columns)
+  (let ((lines
+         (measure-time examine-lines
+          (save-excursion
+            ;;(goto-char (window-start))
+            (goto-char (point-min))
+            (cl-loop
+             for line = (1- (line-number-at-pos)) then (1+ line)
+             ;;until (>= (point) (or (window-end) (point-max)))
+             until (>= (point) (point-max))
+             for line-start = (point)
+             do (end-of-line)
+             for columns = (- (point) line-start)
+             collect (list line line-start columns)
+             do (forward-line)))))
+        (font-width (window-font-width))
+        (font-height (window-font-height))
+        (window-start-line (line-number-at-pos (window-start)))
+        (window-end-line   (line-number-at-pos (window-end))))
+    ;; 2. for each line, generate an overlay & add the image
+    (measure-time add-overlays
+     (overlay-recenter (point-max))
+     (cl-loop
+      for (line position columns) in lines
+      ;; when (<= window-start-line line window-end-line)
+      do
+      (let ((overlay (make-overlay position position))
+            (text    (copy-sequence "x\n"))
+            (hats    (gethash line line-hats '()))
+            (image (cursorless-line-svg columns
+                                        (gethash line line-hats '())
+                                        font-width font-height)))
+        (put-text-property 0 (length text) 'line-height t text)
+        (put-text-property 0 1 'display image text)
+        (overlay-put overlay 'cursorless t)
+        (overlay-put overlay 'before-string text)))))
+
+  ;; OLD VERSION
+  ;; (let ((font-width (window-font-width))
+  ;;       (font-height (window-font-height))
+  ;;       (window-start-line (line-number-at-pos (window-start)))
+  ;;       (window-end-line   (line-number-at-pos (window-end))))
+  ;;   (save-excursion
+  ;;     ;(goto-char (window-start))
+  ;;     (goto-char (point-min)) (overlay-recenter (point-max))
+  ;;     (cl-loop
+  ;;      for line = (1- (line-number-at-pos)) then (1+ line)
+  ;;      ;;until (>= (point) (or (window-end) (point-max)))
+  ;;      until (>= (point) (point-max))
+  ;;      ;; THIS IS SLOW :(
+  ;;      for columns = (num-columns)
+  ;;      for overlay = (make-overlay (point) (point))
+  ;;      for text    = (copy-sequence "x\n")
+  ;;      for hats    = (gethash line line-hats '())
+  ;;      ;; when (<= window-start-line line window-end-line)
+  ;;      do
+  ;;      (let ((image (cursorless-line-svg columns
+  ;;                                        (gethash line line-hats '())
+  ;;                                        font-width font-height)))
+  ;;        (put-text-property 0 (length text) 'line-height t text)
+  ;;        (put-text-property 0 1 'display image text)
+  ;;        (overlay-put overlay 'cursorless t)
+  ;;        (overlay-put overlay 'before-string text))
+  ;;      do
+  ;;      (forward-line))))
+  )
+
+(defun cursorless-line-svg (columns hats font-width font-height)
+  (let* ((w font-width)
+         (dia (* w 0.5)) (h (* 0.6 font-height)) (r (/ dia 2)) (ypos (- h (* r 2)))
          ;; (dia (* w .45)) (h (* w 0.8)) (r (/ dia 2)) (ypos (- h (* r 1.5)))
          (dia (* w .44)) (r (* 0.5 dia)) (h (+ 2 (round dia))) (ypos (- h (* r 1) 1))
-         )
-    ;; hats is a list ((color line offset) (color line offset) ...)
+         ;; if columns = 0, we still want a 1-pixel-wide image, otherwise we get
+         ;; a weird 'empty box' image out of emacs.
+         (svg (svg-create (max 1 (* w columns)) h)))
+    ;; hats is a list of plists with at least the properties :column & :color.
     (dolist (hat hats)
-      (cl-destructuring-bind (color line column) hat
-        (let* ((color (or (cdr (assoc color cursorless-color-alist))
-                          (symbol-name color)))
-               (svg (or (gethash line cursorless-hat-images)
-                        (let ((columns (save-excursion
-                                         (save-restriction
-                                           (widen)
-                                           (goto-char (point-min))
-                                           (forward-line line)
-                                           (let ((before (point)))
-                                             (end-of-line)
-                                             (- (point) before))))))
-                          (puthash line (svg-create (* w columns) h)
-                                   cursorless-hat-images)))))
-          (svg-circle svg (+ (* w column) (/ w 2.0)) ypos r :fill color))))
-    cursorless-hat-images))
-
-(defun cursorless-update-overlays (hats)
-  (cursorless-clear-overlays)
-  (maphash 'cursorless-add-overlay-to-line (cursorless-hat-images hats)))
-
-(defun cursorless-add-overlay-to-line (line svg)
-  (save-excursion
-    (goto-char (point-min))
-    (forward-line line)
-    (let* ((overlay (make-overlay (point) (point)))
-           (text (copy-sequence "x\n"))
-           (image (svg-image svg :scale 1)))
-      (put-text-property 0 (length text) 'line-height t text)
-      (put-text-property 0 1 'display image text)
-      (overlay-put overlay 'cursorless t)
-      (overlay-put overlay 'before-string text))))
-
-
-;; ;;; ---------- NEW VERSION ----------
-;; ;; hats are stored as a JSON object like:
-;; ;; {
-;; ;;   FILEPATH: {
-;; ;;     COLORNAME: [ { "start": { "line": l, "character": c },
-;; ;;                    "end": { "line": l, "character": c } },
-;; ;;                  ... more hat positions ... ]
-;; ;;     ... more colors ...
-;; ;;   },
-;; ;;   ... more files ...
-;; ;; }
-;; ;;
-;; ;; when hats have shapes, COLORNAME is altered (TODO: how).
-;; (defun cursorless-update-hats ()
-;;   (let* ((all-hats (cursorless-read-hats-json))
-;;          ;; we reindex by line since that's how we'll need them
-;;          (line-hats (make-hash-table)))
-;;     (cl-loop
-;;      ;; for now we assume there's only one file, so we just grab the cdar.
-;;      for (color . hats) in (cdar all-hats)
-;;      do (cl-loop
-;;          for hat across hats
-;;          ;; assume each hat's start & end position are the same; we only use the
-;;          ;; start.
-;;          for position = (alist-get 'start hat)
-;;          for line     = (alist-get 'line position)
-;;          for column   = (alist-get 'character position)
-;;          for rest     = (gethash line line-hats '())
-;;          for hat     = `(:column ,column
-;;                          :color ,(or (cdr (assoc color cursorless-color-alist))
-;;                                      (symbol-name color)))
-;;          do (puthash line (cons hat rest) line-hats)))
-;;     ;; now, for every line, generate an image.
-;;     (cursorless-clear-overlays)
-;;     (save-excursion
-;;       (goto-char (point-min))
-;;       (cl-loop
-;;        for line = 0 then (1+ line)
-;;        until (= (point) (point-max))
-;;        for columns = (- (save-excursion (end-of-line) (point)) (point))
-;;        for overlay = (make-overlay (point) (point))
-;;        for text    = (copy-sequence "x\n")
-;;        for hats    = (gethash line line-hats '())
-;;        when hats do
-;;        (let ((image (cursorless-line-svg columns (gethash line line-hats '()))))
-;;          (put-text-property 0 (length text) 'line-height t text)
-;;          (put-text-property 0 1 'display image text)
-;;          (overlay-put overlay 'cursorless t)
-;;          (overlay-put overlay 'before-string text))
-;;        do
-;;        (forward-line)))))
-
-;; (defun cursorless-line-svg (columns hats)
-;;   (let* ((w (window-font-width))
-;;          (dia (* w 0.5)) (h (* 0.6 (window-font-height))) (r (/ dia 2)) (ypos (- h (* r 2)))
-;;          ;; (dia (* w .45)) (h (* w 0.8)) (r (/ dia 2)) (ypos (- h (* r 1.5)))
-;;          (dia (* w .44)) (r (* 0.5 dia)) (h (+ 2 (round dia))) (ypos (- h (* r 1) 1))
-;;          ;; if columns = 0, we still want a 1-pixel-wide image, otherwise we get
-;;          ;; a weird 'empty box' image out of emacs.
-;;          (svg (svg-create (max 1 (* w columns)) h)))
-;;     ;; hats is a list of plists with at least the properties :column & :color.
-;;     (dolist (hat hats)
-;;       (svg-circle svg
-;;                   (+ (/ w 2.0) (* w (plist-get hat :column)))
-;;                   ypos
-;;                   r
-;;                   :fill (plist-get hat :color)))
-;;     ;; scale 1 because we've already accounted for pixel sizes correctly.
-;;     (svg-image svg :scale 1)))
+      (svg-circle svg
+                  (+ (/ w 2.0) (* w (plist-get hat :column)))
+                  ypos
+                  r
+                  :fill (plist-get hat :color)))
+    ;; scale 1 because we've already accounted for pixel sizes correctly.
+    (svg-image svg :scale 1)))
 
 
 ;; ;; what is hats? a list ((color line offset) (color line offset) ...)
