4949b21f72b877f61e873ecbd5fc72b566d03e2e
dyamito942 <dyamito942@gmail.com>
2022-08-27 13:49:57 -0400

    Prefer defconst

--- cursorless.el
+++ cursorless.el
@@ -168,9 +168,9 @@
     (file-notify-add-watch cursorless-hats-file '(change) 'cursorless-hats-change-callback)))
 
 ;; TODO: defcustom
-(defparam cursorless-color-alist
+(defconst cursorless-color-alist
   '((default . "#999") (blue . "#04f") (red . "dark red") (pink . "coral") (green . "#0b0")))
-(defparam cursorless-color-alist ; dark theme
+(defconst cursorless-color-alist ; dark theme
   '((default . "#999") (blue . "#0af") (red . "#f00") (pink . "#fa8072") (green . "#0a0")))
 
 (defun show-hats ()
