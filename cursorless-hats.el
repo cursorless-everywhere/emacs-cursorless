2da6fd8977fd67e5b1575ef48b32053dbe6089ca
dyamito942 <dyamito942@gmail.com>
2022-09-04 05:32:23 -0400

    Native hat drawing implementation

--- cursorless-hats.el
+++ cursorless-hats.el
@@ -1,3 +1,5 @@
+(require 'dash)
+
 ;; READING & DRAWING HATS FROM CURSORLESS
 (defconst cursorless-hats-file
   (concat cursorless-directory "vscode-hats.json"))
@@ -30,6 +32,7 @@
        (setq cursorless-hats-update-timer nil)
        (when cursorless-show-hats (cursorless-update-hats)))
      (setq cursorless-updating-hats nil))))
+
 (defun cursorless-hats-change-callback (&optional event)
   (when cursorless-updating-hats
     (message "cursorless-hats CHANGE RECURSIVE, set cursorless-updating-hats to nil to re-enable"))
@@ -48,7 +51,6 @@
   (interactive)
   (when cursorless-show-hats (cursorless-clear-overlays))
   (setq cursorless-show-hats t)
-  (cursorless-initialize-hats)
   (cursorless-hats-update-callback))
 
 ;;; FIXME: need to deinitialize hats in all buffers which have them.
@@ -63,124 +65,72 @@
    (remove-overlays nil nil 'cursorless t)))
 
 (defun cursorless-read-hats-json ()
+  "Read the hats file and return an alist.
+
+Hats are stored as a JSON object like:
+{
+  FILEPATH: {
+    COLORNAME: [ { \"start\": { \"line\": l, \"character\": c },
+                   \"end\": { \"line\": l, \"character\": c } },
+                 ... more hat positions ... ]
+    ... more colors ...
+  },
+  ... more files ...
+}
+
+COLORNAME is sometimes a color name e.g. blue and sometimes a color followed
+by a shape e.g. blue-bolt."
   (with-temp-buffer
     (insert-file-contents-literally cursorless-hats-file)
     (json-parse-buffer :object-type 'alist)))
 
-
-;;; ---------- NEW VERSION ----------
-;; hats are stored as a JSON object like:
-;; {
-;;   FILEPATH: {
-;;     COLORNAME: [ { "start": { "line": l, "character": c },
-;;                    "end": { "line": l, "character": c } },
-;;                  ... more hat positions ... ]
-;;     ... more colors ...
-;;   },
-;;   ... more files ...
-;; }
-;;
-;; when hats have shapes, COLORNAME is altered (TODO: how).
-(defvar cursorless-hats-json)
 (defvar cursorless-hats-buffer nil)
 
 (defun cursorless-update-hats ()
-  ;; TODO: need to _actually_ use the file path info from the hats file.
-  ;; to do this we need a reverse map from temp files to buffers.
+  "Update the relevant buffer with the latest hats."
   (let* ((json (cursorless-read-hats-json))
+         ;; TODO: only looks at the first file in the hats json.
          (temporary-file (and (caar json) (symbol-name (caar json))))
          (buffer (gethash temporary-file cursorless-temporary-file-buffers)))
     (cond
      ((null json)
       (message "cursorless-update-hats: vscode-hats.json contained empty object."))
      ((null temporary-file)
-      (warn "could not extract temporary file name from json")
-      (setq cursorless-hats-json json))
+      (warn "could not extract temporary file name from json"))
      ((null buffer)
       (warn "temporary file not associated with a buffer: %S" temporary-file)))
+    ;; clear the previous hat buffers overlays
     (unless (equal buffer cursorless-hats-buffer)
       (when (and cursorless-hats-buffer (buffer-live-p cursorless-hats-buffer))
-        (with-current-buffer cursorless-hats-buffer (cursorless-clear-overlays)))
-      (when (and buffer (buffer-live-p buffer))
-        (with-current-buffer buffer (cursorless-initialize-hats))))
+        (with-current-buffer cursorless-hats-buffer (cursorless-clear-overlays))))
     (setq cursorless-hats-buffer buffer)
-    (measure-time "update hats"
-      (cursorless-update-overlays
-        (measure-time "read/index hats"
-          (cursorless-index-hats json))))))
+    (cursorless-clear-overlays)
+    (-map (lambda(color-positions)
+            (let ((draw-hat (-partial 'cursorless-draw-hat (car color-positions))))
+              (-map draw-hat (cdr color-positions)))) (cdar json))))
 
-(defvar cursorless-hats (make-hash-table))
-(defun cursorless-index-hats (hats-json)
-  (clrhash cursorless-hats)
-  (cl-loop
-   ;; for now we assume there's only one file, so we just grab the cdar.
-   ;; FIXME: use cursorless-temporary-file-buffers instead!
-   for (color . hats) in (cdar hats-json)
-   do (cl-loop
-       for hat across hats
-       ;; assume each hat's start & end position are the same; we only use the
-       ;; start.
-       for position = (alist-get 'start hat)
-       for line     = (alist-get 'line position)
-       for column   = (alist-get 'character position)
-       for rest     = (gethash line cursorless-hats '())
-       for hat     = `(:column ,column
-                       :color ,(or (cdr (assoc color cursorless-color-alist))
-                                   (symbol-name color)))
-       do (puthash line (cons hat rest) cursorless-hats)))
-  cursorless-hats)
+(defun point-from-cursorless-position (cursorless-position)
+  "Return the proper point for an alist from a cursorless position.
 
-(defun cursorless-initialize-hats ()
-  (cursorless-update-overlays-between (make-hash-table) (point-min) (point-max)))
+CURSORLESS-POSITION is an alist parsed from `cursorless-read-hats-json'."
+  (let ((pos (alist-get 'start cursorless-position)))
+    (save-excursion
+      (goto-char (point-min))
+      (forward-line (alist-get 'line pos))
+      (forward-char (alist-get 'character pos))
+      (point))))
 
-(defun cursorless-update-overlays (line-hats)
-  (cursorless-update-overlays-between line-hats (window-start) (window-end)))
+(defun cursorless-draw-hat (cursorless-color cursorless-position)
+  "Draw an individual hat on the current buffer.
 
-(defun cursorless-update-overlays-between (line-hats &optional start end)
-  (measure-time "update overlays"
-   (remove-overlays start end 'cursorless t)
-   (let ((font-width  (window-font-width))
-         (font-height (window-font-height)))
-     (save-excursion
-       (goto-char (or start (point-min)))
-       (cl-loop
-        for line = (1- (line-number-at-pos)) then (1+ line)
-        until (or (eobp) (>= (point) end))
-        for line-start = (point)
-        do (end-of-line)
-        for columns = (- (point) line-start)
-        do (let* ((overlay (make-overlay line-start line-start))
-                  (text    (copy-sequence "x\n"))
-                  (hats    (gethash line line-hats '()))
-                  (image   (cursorless-line-svg columns hats font-width font-height)))
-             (put-text-property 0 (length text) 'line-height t text)
-             (put-text-property 0 1 'display image text)
-             (overlay-put overlay 'cursorless t)
-             (overlay-put overlay 'before-string text))
-        do (forward-line))))))
+CURSORLESS-COLOR is a color name (e.g. default, blue, pink) which is sometimes
+followed by a hat shape e.g. default-bolt, blue-fox, pink-frame.
 
-;; FIXME: multiplying character width by character offset doesn't work for
-;; strange-width characters (eg. TABS!).
-(defun cursorless-line-svg (columns hats font-width font-height)
-  (let* ((w font-width)
-         ;; (dia (* w 0.5)) (h (* 0.6 font-height)) (r (/ dia 2)) (ypos (- h (* r 2)))
-         ;; (dia (* w .45)) (h (* w 0.8)) (r (/ dia 2)) (ypos (- h (* r 1.5)))
-         (dia (* w .44)) (r (* 0.5 dia)) (h (+ 2 (round dia))) (ypos (- h (* r 1) 1))
-         (dia (* w .44)) (r (* 0.5 dia)) (h (ceiling dia)) (ypos (- h r))
-         ;; if columns = 0, we still want a 1-pixel-wide image, otherwise we get
-         ;; a weird 'empty box' image out of emacs.
-         (svg (svg-create (max 1 (* w columns)) h)))
-    ;; hats is a list of plists with at least the properties :column & :color.
-    (dolist (hat hats)
-      (let* ((color (plist-get hat :color))
-             (xoffset (* w (plist-get hat :column)))
-             (xcenter (+ xoffset (/ w 2.0))))
-        ;(svg-circle svg xcenter ypos r :fill color)
-        ;; squashed ellipse to save vertical space.
-        (svg-ellipse svg xcenter ypos r (/ (- h 1) 2.0) :fill color)
-        ;(svg-rectangle svg (- xcenter r) 1 dia dia :fill color)
-        ))
-    ;; scale 1 because we've already accounted for pixel sizes correctly.
-    (svg-image svg :scale 1)))
+CURSORLESS-POSITION is an alist parsed from `cursorless-read-hats-json'."
+  (when-let* ((hat-color (alist-get cursorless-color cursorless-color-alist))
+              (hat-point (point-from-cursorless-position cursorless-position))
+              (hat-overlay (make-overlay hat-point (+ hat-point 1))))
+    (overlay-put hat-overlay 'cursorless t)
+    (overlay-put hat-overlay 'face `(:cursorless ,hat-color))))
 
 (provide 'cursorless-hats)
